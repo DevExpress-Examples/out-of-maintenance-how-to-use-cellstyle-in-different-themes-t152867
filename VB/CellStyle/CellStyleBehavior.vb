dd7f923eda3e33f86d60bf288cd193cc0e5fe559
DevExpressExampleBot <fakeEmail>
2022-12-13 16:46:01 +0400

    autogenerate VB

--- VB/CellStyle/CellStyleBehavior.vb
+++ VB/CellStyle/CellStyleBehavior.vb
@@ -1,7 +1,3 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Grid
 Imports DevExpress.Xpf.Core
 Imports System.Windows
@@ -9,55 +5,54 @@ Imports DevExpress.Xpf.Grid.Themes
 Imports DevExpress.Mvvm.UI.Interactivity
 
 Namespace CellStyle
+
     Public Class CellStyleBehavior
         Inherits Behavior(Of GridControl)
 
-        Public Shared CellStyleProperty As DependencyProperty = DependencyProperty.Register("CellStyle", GetType(Style), GetType(CellStyleBehavior), New PropertyMetadata(New Style() With {.TargetType=GetType(LightweightCellEditor)}))
+        Public Shared CellStyleProperty As DependencyProperty = DependencyProperty.Register("CellStyle", GetType(Style), GetType(CellStyleBehavior), New PropertyMetadata(New Style() With {.TargetType = GetType(LightweightCellEditor)}))
 
-        Public Property CellStyle() As Style
+        Public Property CellStyle As Style
             Get
-                Return CType(GetValue(CellStyleProperty), Style)
+                Return CType(Me.GetValue(CellStyle.CellStyleBehavior.CellStyleProperty), Style)
             End Get
+
             Set(ByVal value As Style)
                 If value.TargetType Is GetType(LightweightCellEditor) Then
-                    SetValue(CellStyleProperty, value)
+                    Me.SetValue(CellStyle.CellStyleBehavior.CellStyleProperty, value)
                     ApplyStyleToGrid(ThemeManager.ApplicationThemeName)
                 End If
             End Set
         End Property
 
         Private grid As GridControl
+
         Protected Overrides Sub OnAttached()
             MyBase.OnAttached()
             Try
-                grid = TryCast(Me.AssociatedObject, GridControl)
-                If grid IsNot Nothing Then
-                    AddHandler ThemeManager.ThemeChanged, AddressOf ThemeManager_ThemeChanged
-                End If
+                grid = TryCast(AssociatedObject, GridControl)
+                If grid IsNot Nothing Then AddHandler ThemeManager.ThemeChanged, New ThemeChangedRoutedEventHandler(AddressOf ThemeManager_ThemeChanged)
             Finally
             End Try
         End Sub
 
         Private Function CopyStyle(ByVal originalStyle As Style) As Style
-            Dim copiedStyle As New Style()
+            Dim copiedStyle As Style = New Style()
             copiedStyle.TargetType = originalStyle.TargetType
-
             For Each elem In originalStyle.Setters
                 copiedStyle.Setters.Add(elem)
-            Next elem
+            Next
 
             For Each elem In originalStyle.Triggers
                 copiedStyle.Triggers.Add(elem)
-            Next elem
+            Next
+
             Return copiedStyle
         End Function
 
         Private Sub ApplyStyleToGrid(ByVal themeName As String)
-            Dim newKey As New GridRowThemeKeyExtension()
+            Dim newKey As GridRowThemeKeyExtension = New GridRowThemeKeyExtension()
             newKey.ResourceKey = GridRowThemeKeys.LightweightCellStyle
-            If themeName <> "DeepBlue" Then
-                newKey.ThemeName = themeName
-            End If
+            If Not Equals(themeName, "DeepBlue") Then newKey.ThemeName = themeName
             Dim currStyle As Style = CopyStyle(CellStyle)
             currStyle.BasedOn = TryCast(Window.GetWindow(grid).FindResource(newKey), Style)
             grid.View.CellStyle = currStyle
