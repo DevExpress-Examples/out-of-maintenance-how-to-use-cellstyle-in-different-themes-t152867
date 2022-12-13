13ffd07b77536ed2762571ea0a97c0f569b57870
DevExpressExampleBot <fakeEmail>
2022-12-13 20:40:21 +0400

    Source auto update [skip ci]

--- VB/CellStyle/Data.vb
+++ VB/CellStyle/Data.vb
@@ -1,24 +1,26 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 
 Namespace CellStyle
+
     Public Class Data
-        Public Shared ReadOnly Property DataList() As List(Of Node)
+
+        Public Shared ReadOnly Property DataList As List(Of Node)
             Get
-                Dim list As New List(Of Node)()
-                For i As Integer = 0 To 9
+                Dim list As List(Of Node) = New List(Of Node)()
+                For i As Integer = 0 To 10 - 1
                     list.Add(New Node("key" & i, "line " & i))
-                Next i
+                Next
+
                 Return list
             End Get
         End Property
     End Class
 
     Public Class Node
-        Public Property Key() As String
-        Public Property Text() As String
+
+        Public Property Key As String
+
+        Public Property Text As String
 
         Public Sub New(ByVal key As String, ByVal text As String)
             Me.Key = key
--- VB/CellStyle/MainWindow.xaml.vb
+++ VB/CellStyle/MainWindow.xaml.vb
@@ -1,38 +1,18 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
-Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Core
-Imports DevExpress.Xpf.Grid.Themes
+Imports DevExpress.Xpf.Editors
 
 Namespace CellStyle
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            grid1.ItemsSource = Data.DataList
-        End Sub
-
-        Private Sub themesComboBox_SelectionChanged(ByVal sender As Object, ByVal e As SelectionChangedEventArgs)
-            ThemeManager.ApplicationThemeName = (TryCast(e.AddedItems(0), Theme)).Name
+        Private Sub EditValueChanged(ByVal sender As Object, ByVal e As EditValueChangedEventArgs)
+            ThemeManager.ApplicationThemeName = TryCast(e.NewValue, Theme)?.Name
         End Sub
     End Class
-
-
 End Namespace
